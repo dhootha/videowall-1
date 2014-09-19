af9aba9af35ed2f504d915fc13bac943b825fc64
Fraser <fraser.chapman@gmail.com>
2014-09-19 23:39:04 +0100

    loop folder

--- loop.sh
+++ loop.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+VIDEOPATH="/home/window/videos"
+SERVICE="avconv"
+BROADCAST="udp://239.0.1.23:1234"
+
 while true;
 do
-  avconv -re -i $file -vcodec copy -f avi udp://239.0.1.23:1234
+  for file in $VIDEOPATH/*
+    $SERVICE -re -i $file -vcodec copy -f avi $BROADCAST
 done
 
 exit 0
